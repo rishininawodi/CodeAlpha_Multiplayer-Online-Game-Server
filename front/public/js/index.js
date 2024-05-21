f7ee8cceafdd4cd02aa507d59b7e95a1c269d660
Rishini <nawodirishini88@gmail.com>
2024-05-21 10:16:37 +0530

    I started to create my chess game server.Stiil not completed

---
+++ front/public/js/index.js
@@ -0,0 +1,125 @@
+
+let gameHasStarted = false;
+var board = null
+var game = new Chess()
+var $status = $('#status')
+var $pgn = $('#pgn')
+let gameOver = false;
+
+function onDragStart (source, piece, position, orientation) {
+    
+    if (game.game_over()) return false
+    if (!gameHasStarted) return false;
+    if (gameOver) return false;
+
+    if ((playerColor === 'black' && piece.search(/^w/) !== -1) || (playerColor === 'white' && piece.search(/^b/) !== -1)) {
+        return false;
+    }
+
+    
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) || (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false
+    }
+}
+
+function onDrop (source, target) {
+    let theMove = {
+        from: source,
+        to: target,
+        promotion: 'q' 
+    };
+   
+    var move = game.move(theMove);
+
+
+  
+    if (move === null) return 'snapback'
+
+    socket.emit('move', theMove);
+
+    updateStatus()
+}
+
+socket.on('newMove', function(move) {
+    game.move(move);
+    board.position(game.fen());
+    updateStatus();
+});
+
+
+function onSnapEnd () {
+    board.position(game.fen())
+}
+
+function updateStatus () {
+    var status = ''
+
+    var moveColor = 'White'
+    if (game.turn() === 'b') {
+        moveColor = 'Black'
+    }
+
+
+    if (game.in_checkmate()) {
+        status = 'Game over, ' + moveColor + ' is in checkmate.'
+    }
+
+   
+    else if (game.in_draw()) {
+        status = 'Game over, drawn position'
+    }
+
+    else if (gameOver) {
+        status = 'Opponent disconnected, you win!'
+    }
+
+    else if (!gameHasStarted) {
+        status = 'Waiting for black to join'
+    }
+
+   
+    else {
+        status = moveColor + ' to move'
+
+        
+        if (game.in_check()) {
+            status += ', ' + moveColor + ' is in check'
+        }
+        
+    }
+
+    $status.html(status)
+    $pgn.html(game.pgn())
+}
+
+var config = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd,
+    pieceTheme: '/public/img/chesspieces/wikipedia/{piece}.png'
+}
+board = Chessboard('myBoard', config)
+if (playerColor == 'black') {
+    board.flip();
+}
+
+updateStatus()
+
+var urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get('code')) {
+    socket.emit('joinGame', {
+        code: urlParams.get('code')
+    });
+}
+
+socket.on('startGame', function() {
+    gameHasStarted = true;
+    updateStatus()
+});
+
+socket.on('gameOverDisconnect', function() {
+    gameOver = true;
+    updateStatus()
+});
